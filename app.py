e3ac176c93b003bf40384afe660a63dfa3b5c511
techblogp <techblogp@gmail.com>
2020-11-10 10:58:22 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import spacy_streamlit
+import spacy
+nlp = spacy.load("en_core_web_sm")
 
-models = ["en_core_web_sm", "en_core_web_md"]
+# models = ["en_core_web_sm", "en_core_web_md"]
 default_text = "Sundar Pichai is the CEO of Google."
-spacy_streamlit.visualize(models, default_text)
+# spacy_streamlit.visualize(models, default_text)
